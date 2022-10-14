d90c8b4d4ff93cdf61c028d68fa391cd4408dfce
davidanthoff <davidanthoff@users.noreply.github.com>
2022-10-14 09:32:12 +0000

    Format files using DocumentFormat

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,13 @@
 makedocs(
-    modules = [MimiMooreEtAlAgricultureImpacts],
-    sitename = "Moore Agriculture Documentation",
-    pages = [
+    modules=[MimiMooreEtAlAgricultureImpacts],
+    sitename="Moore Agriculture Documentation",
+    pages=[
         "Home" => "index.md",
         "Reference" => "reference.md"
     ],
-    format = Documenter.HTML(prettyurls = get(ENV, "JULIA_NO_LOCAL_PRETTY_URLS", nothing) === nothing)
+    format=Documenter.HTML(prettyurls=get(ENV, "JULIA_NO_LOCAL_PRETTY_URLS", nothing) === nothing)
 )
 
 deploydocs(
-    repo = "https://github.com/ckingdon95/MimiMooreEtAlAgricultureImpacts.jl.git",
+    repo="https://github.com/ckingdon95/MimiMooreEtAlAgricultureImpacts.jl.git",
 )
